6b4b4683cb2027a36c973a37681184e6c87c77d6
Alfredo <a.vbarrio@hotmail.com>
2017-01-16 10:46:24 +0100

    Cambios

--- Fusion_System/Fusion_System.u/local_interfaces/Common.h
+++ Fusion_System/Fusion_System.u/local_interfaces/Common.h
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <string>
 
-#define idLaser 0//71
-#define idCamera 0//11
+#define idLaser 71
+#define idCamera 11
 #define idEst 0//11
 #define dir "D:\\Escritorio\\Graficas\\Result\\"
